6f8e0f477522c3c8cf43d5cf442ea220366dc52b
jared beckler and jordan meier <jared.beckler@gmail.com>
2016-04-05 15:14:43 -0700

    change the way header is displayed on legislator page

--- app/routes/legislator.js
+++ app/routes/legislator.js
@@ -5,9 +5,10 @@ export default Ember.Route.extend({
   model(params) {
     var key = config.myApiKey;
     var legislatorUrl = 'http://congress.api.sunlightfoundation.com/legislators?bioguide_id=' + params.bioguide_id +'&apikey=' + key;
-    var url = 'http://congress.api.sunlightfoundation.com/committees?member_ids=' + params.bioguide_id +'&apikey=' + key;
+    var committeeUrl = 'http://congress.api.sunlightfoundation.com/committees?member_ids=' + params.bioguide_id +'&apikey=' + key;
+    
     return Ember.RSVP.hash({
-      committees: Ember.$.getJSON(url).then(function(responseJSON) {
+      committees: Ember.$.getJSON(committeeUrl).then(function(responseJSON) {
         return responseJSON.results;
       }),
 
