d8ce0018511f153615bf5c1602511bf5cfd14393
jared beckler and jordan meier <jared.beckler@gmail.com>
2016-04-05 15:49:10 -0700

    change name of parameter being passed to model hook in legislator route handler

--- app/routes/legislator.js
+++ app/routes/legislator.js
@@ -2,11 +2,12 @@ import config from '../config/environment';
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model(params) {
+  //you can pass anything into the model below, as long as it matches in the url variables. Could be called params, model, bob, whatever.
+  model(legislator) {
     var key = config.myApiKey;
-    var legislatorUrl = 'http://congress.api.sunlightfoundation.com/legislators?bioguide_id=' + params.bioguide_id +'&apikey=' + key;
-    var committeeUrl = 'http://congress.api.sunlightfoundation.com/committees?member_ids=' + params.bioguide_id +'&apikey=' + key;
-    
+    var legislatorUrl = 'http://congress.api.sunlightfoundation.com/legislators?bioguide_id=' + legislator.bioguide_id +'&apikey=' + key;
+    var committeeUrl = 'http://congress.api.sunlightfoundation.com/committees?member_ids=' + legislator.bioguide_id +'&apikey=' + key;
+
     return Ember.RSVP.hash({
       committees: Ember.$.getJSON(committeeUrl).then(function(responseJSON) {
         return responseJSON.results;
